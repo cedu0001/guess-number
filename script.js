d94f27ceca6a95c5cf0b2bc79ea79a456cb97591
Celia <cmarianielsen@gmail.com>
2026-01-29 12:37:41 +0100

    rettet

--- script.js
+++ script.js
@@ -10,33 +10,41 @@ Det skal se godt ud
 Det skal give relevant feedback */
 "use strict";
 
+//Laver en variabel som genererer et tal
 const number = Math.ceil(Math.random() * 100);
+
+//Laver en variabel som selecter #input
 const input = document.querySelector("#input");
+
+//Laver en variabel som selecter #btn
 const button = document.querySelector("#btn");
 
+//Lytter til når man klikker på knappen, den referer så til funktionen guessNumber
 button.addEventListener("click", guessNumber);
 
 console.log(input);
 console.log(number);
 
+
+//animation funktion
 function animateBoxes(){
     document.querySelector("#billeder").classList.remove("hide");
     document.querySelector("#billeder").classList.add("makeCircular");
 }
 
+//sætter billedet ind
 function billede1 (){
     document.querySelector("#billeder").innerHTML = "<img src='/img/happy-cat.jpg' alt=''>"
 
 }
 
+//sætter billedet ind
 function billede2 (){
     document.querySelector("#billeder").innerHTML = "<img src='/img/sad-cat.webp' alt=''>"
  
 }
 
-
- 
-
+//
 function guessNumber (e) {
     console.log("input.value type:", typeof input.value);
     console.log("Tilfældig tal:", number);
@@ -45,6 +53,7 @@ function guessNumber (e) {
         alert("Tillyke! Du har gættet rigtigt! :D");
         billede1();
            animateBoxes();
+           
         
     }else{
         e.preventDefault();
